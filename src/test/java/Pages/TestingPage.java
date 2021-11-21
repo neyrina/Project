441ea098a8dd30a97968f973a49a13f0a622ec7a
neyrina <89096299+neyrina@users.noreply.github.com>
2021-11-21 22:13:13 +0300

    commit

--- src/test/java/Pages/TestingPage.java
+++ src/test/java/Pages/TestingPage.java
@@ -26,37 +26,37 @@ public class TestingPage extends BasePage {
         List<WebElement> courses = driver.findElements(By.xpath("//div[@class=\"lessons\"]//a[@class=\"js-stats lessons__new-item lessons__new-item_hovered\"]"));
         WebDriverWait wait = new WebDriverWait(driver, 5);
 
-        for (int i = 1; i <=courses.size(); i++) {
+        for (int i = 1; i <= courses.size(); i++) {
             courses = driver.findElements(By.xpath("//div[@class=\"lessons\"]//a[@class=\"js-stats lessons__new-item lessons__new-item_hovered\"]"));
-            wait.until(ExpectedConditions.visibilityOf(courses.get(i-1)));
-            String courseUrl = courses.get(i-1).getAttribute("href");
+            wait.until(ExpectedConditions.visibilityOf(courses.get(i - 1)));
+            String courseUrl = courses.get(i - 1).getAttribute("href");
             driver.get(courseUrl);
             switch (courseUrl) {
-                case "https://otus.ru/lessons/qa-auto-java-specialization/?int_source=courses_catalog&int_term=testing":
-                case "https://otus.ru/online/manualtesting/":
-                    driver.navigate().back();//Эти две страницы имеют вообще другой формат. Просто пропускаю
-                    break;
-                case "https://otus.ru/lessons/java-qa-pro/?int_source=courses_catalog&int_term=testing"://Т.к локаторы на страницах отличаются, пришлось наплодить кэйсов
-                case "https://otus.ru/lessons/avtomatizaciya-web-testirovaniya/?int_source=courses_catalog&int_term=testing":
-                    Assert.assertFalse("Не указан заголовок курса", driver.findElement(By.xpath("//div[@class='course-header2__title']")).getText().isEmpty());
-                    Assert.assertFalse("Не указано описание курса", driver.findElement(By.xpath("//div[@class='course-header2__admin-text']")).getText().isEmpty());
+                case "https://otus.ru/lessons/java-qa-pro/?int_source=courses_catalog&int_term=testing"://Т.к локаторы на страницах отличаются, пришлось наплодить кейсов
+                case "https://otus.ru/lessons/avtomatizaciya-web-testirovaniya/?int_source=courses_catalog&int_term=testing"://решила оставить ссылки, т.к удобнее сразу кликать на нее для проверки. А с енам что-то не сложилось в этот раз
+                    Assert.assertFalse("Не указан заголовок курса " + courseUrl, driver.findElement(By.xpath("//div[@class='course-header2__title']")).getText().isEmpty());
+                    Assert.assertFalse("Не указано описание курса " + courseUrl, driver.findElement(By.xpath("//div[@class='course-header2__admin-text']")).getText().isEmpty());
                     commonCheck();
                     break;
                 case "https://otus.ru/lessons/java-qa-basic/?int_source=courses_catalog&int_term=testing":
                 case "https://otus.ru/lessons/qajs/?int_source=courses_catalog&int_term=testing":
-                    Assert.assertFalse("Не указан заголовок курса", driver.findElement(By.xpath("//div[@class='course-header2__title']")).getText().isEmpty());
-                    Assert.assertFalse("Не указано описание курса", driver.findElement(By.xpath("//h1[@class='course-header2__subtitle']")).getText().isEmpty());
+                    Assert.assertFalse("Не указан заголовок курса " + courseUrl, driver.findElement(By.xpath("//div[@class='course-header2__title']")).getText().isEmpty());
+                    Assert.assertFalse("Не указано описание курса " + courseUrl, driver.findElement(By.xpath("//h1[@class='course-header2__subtitle']")).getText().isEmpty());
                     commonCheck();
                     break;
                 case "https://otus.ru/lessons/kotlin-qa-engineer/?int_source=courses_catalog&int_term=testing"://в этой карточке отсутствует характеристика Формат
-                    Assert.assertFalse("Не указан заголовок курса", driver.findElement(By.xpath("//div[@class='course-header2__title']")).getText().isEmpty());
-                    Assert.assertFalse("Не указано описание курса", driver.findElement(By.xpath("//div[@class='course-header2__admin-text']")).getText().isEmpty());
-                    Assert.assertFalse("Не указана длительность курса", driver.findElement(By.xpath("//p[@class='course-header2-bottom__item-text']")).getText().isEmpty());
+                    Assert.assertFalse("Не указан заголовок курса " + courseUrl, driver.findElement(By.xpath("//div[@class='course-header2__title']")).getText().isEmpty());
+                    Assert.assertFalse("Не указано описание курса " + courseUrl, driver.findElement(By.xpath("//div[@class='course-header2__admin-text']")).getText().isEmpty());
+                    Assert.assertFalse("Не указана длительность курса " + courseUrl, driver.findElement(By.xpath("//p[@class='course-header2-bottom__item-text']")).getText().isEmpty());
                     driver.navigate().back();
                     break;
+                case "https://otus.ru/lessons/qa-auto-java-specialization/?int_source=courses_catalog&int_term=testing":
+                case "https://otus.ru/online/manualtesting/":
+                    driver.navigate().back();//Эти две страницы имеют вообще другой формат. Просто пропускаю
+                    break;
                 default:
-                    Assert.assertFalse("Не указан заголовок курса", driver.findElement(By.xpath("//h1[@class='course-header2__title']")).getText().isEmpty());
-                    Assert.assertFalse("Не указано описание курса", driver.findElement(By.xpath("//div[@class='course-header2__admin-text']")).getText().isEmpty());
+                    Assert.assertFalse("Не указан заголовок курса " + courseUrl, driver.findElement(By.xpath("//h1[@class='course-header2__title']")).getText().isEmpty());
+                    Assert.assertFalse("Не указано описание курса " + courseUrl, driver.findElement(By.xpath("//div[@class='course-header2__admin-text']")).getText().isEmpty());
                     commonCheck();
                     break;
             }
